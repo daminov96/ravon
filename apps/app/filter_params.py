82d275006be4c28a10d8853e491c0ed0bab0b797
Jonibek Daminov <dovud.daminov@gmail.com>
2021-09-18 09:37:28 +0500

    added any filter params

--- apps/app/filter_params.py
+++ apps/app/filter_params.py
@@ -1,9 +1,37 @@
 from drf_yasg import openapi
 
 
-
 def get_city_params():
     name = openapi.Parameter("name", openapi.IN_QUERY, description='send city name', type=openapi.TYPE_STRING)
 
+    return [name]
+
+
+def get_routine_params():
+    name = openapi.Parameter("name", openapi.IN_QUERY, description='send city name', type=openapi.TYPE_STRING)
 
     return [name]
+
+
+def get_min_price_params():
+    price = openapi.Parameter('price', openapi.IN_QUERY, description='price', type=openapi.TYPE_STRING)
+
+    return [price]
+
+
+def get_brand_params():
+    name = openapi.Parameter("name", openapi.IN_QUERY, description='send brand name', type=openapi.TYPE_STRING)
+
+    return [name]
+
+
+def get_model_params():
+    name = openapi.Parameter("name", openapi.IN_QUERY, description='send model name', type=openapi.TYPE_STRING)
+
+    return [name]
+
+
+def get_car_params():
+    number = openapi.Parameter("number", openapi.IN_QUERY, description='send car number ', type=openapi.TYPE_STRING)
+
+    return [number]
--- apps/app/views.py
+++ apps/app/views.py
@@ -33,28 +33,51 @@ class CityView(ModelViewSet):
     serializer_class = CitySerializer
 
     def get_queryset(self):
-        queryset = self.get_queryset()
+        queryset = super().get_queryset()
         queryparams = self.request.query_params
-
         name = queryparams.get('name', None)
         if name:
-            queryset = queryset.filter(name__incontains=name)
-
+            queryset = queryset.filter(name__icontains=name)
         return queryset
 
     @swagger_auto_schema(manual_parameters=filter_params.get_city_params())
     def list(self, request, *args, **kwargs):
-        return super(CityView).list(**kwargs)
+        return super(CityView, self).list(kwargs)
 
 
 class RoutineView(ModelViewSet):
     queryset = Routine.objects.all()
     serializer_class = RoutineSerializer
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryparams = self.request.query_params
+        name = queryparams.get('name', None)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=filter_params.get_routine_params())
+    def list(self, request, *args, **kwargs):
+        return super(RoutineView, self).list(kwargs)
 
 
 class MinimumPriceKmView(ModelViewSet):
     queryset = MinimumPriceForKm.objects.all()
     serializer_class = MinimumPriceKmSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryparams = self.request.query_params
+
+        price = queryparams.get('price', None)
+        if price:
+            queryset = queryset.filter(price=price)
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=filter_params.get_min_price_params())
+    def list(self, request, *args, **kwargs):
+        return super(MinimumPriceKmView, self).list(kwargs)
 
 
 class PlanView(ModelViewSet):
@@ -73,16 +96,54 @@ class LocationView(ModelViewSet):
 
 
 class CarView(ModelViewSet):
-    # queryset = Car.objects.select_related('brand', 'model').all()
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryparams = self.request.query_params
+        number = queryparams.get('number', None)
+        if number:
+            queryset = queryset.filter(number=number)
+
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=filter_params.get_car_params())
+    def list(self, request, *args, **kwargs):
+        return super(CarView, self).list(kwargs)
 
 
 class BrandView(ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryparams = self.request.query_params
+        name = queryparams.get('name', None)
+        if name:
+            queryset = queryset.filter(name=name)
+
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=filter_params.get_brand_params())
+    def list(self, request, *args, **kwargs):
+        return super(BrandView, self).list(kwargs)
+
 
 class ModelView(ModelViewSet):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryparams = self.request.query_params
+        name = queryparams.get('name', None)
+        if name:
+            queryset = queryset.filter(name=name)
+
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=filter_params.get_model_params())
+    def list(self, request, *args, **kwargs):
+        return super(ModelView, self).list(kwargs)
